dff325f07afa6a28158e4dae46da9c0e2f477946
zidanegimiga <zidanegimigatemp@gmail.com>
2026-03-12 23:58:42 +0300

    styles: set up app theme

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  serverExternalPackages: ["mongodb", "mongoose"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
